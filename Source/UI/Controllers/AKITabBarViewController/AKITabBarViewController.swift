97b96d93d73ef04b54715ef7a9da77d734c12f90
Alexey Khomych <alexeyfurious@gmail.com>
2016-11-01 13:54:55 +0200

    added project

---
+++ Source/UI/Controllers/AKITabBarViewController/AKITabBarViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AKITabBarViewController.swift
+//  Compas
+//
+//  Created by Alexey Khomych on 31.10.16.
+//  Copyright © 2016 Alexey Khomych. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class AKITabBarViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ Source/UI/Controllers/AKITabBarViewController/Views/AKITabBarView.swift
@@ -0,0 +1,15 @@
+//
+//  AKITabBarView.swift
+//  Compas
+//
+//  Created by Alexey Khomych on 31.10.16.
+//  Copyright © 2016 Alexey Khomych. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class AKITabBarView: UIView {
+    @IBOutlet var mapView: MKMapView!
+
+}
